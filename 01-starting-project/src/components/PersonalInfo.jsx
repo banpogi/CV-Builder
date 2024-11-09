dec252a76341cb786e311acdaf223330a962993b
banpogi <banbansoy1@gmail.com>
2024-11-09 22:20:27 +0800

    added styles on the input form

--- 01-starting-project/src/components/PersonalInfo.jsx
+++ 01-starting-project/src/components/PersonalInfo.jsx
@@ -1,7 +1,7 @@
 export default function PersonalInfo({ userInput, onChange }) {
   return (
     <div className="info-container">
-      <div className="info-form">
+      {/* <div className="info-form">
         <label>First Name</label>
         <input
           type="text"
@@ -31,6 +31,34 @@ export default function PersonalInfo({ userInput, onChange }) {
           type="email"
           value={userInput.email}
           required
+          onChange={(event) => onChange("email", event.target.value)}
+        />
+      </div> */}
+      <div className="form-container">
+        <label>First Name</label>
+        <input
+          type="text"
+          value={userInput.firstName}
+          onChange={(event) => onChange("firstName", event.target.value)}
+        />
+
+        <label>Last Name</label>
+        <input
+          type="text"
+          value={userInput.lastName}
+          onChange={(event) => onChange("lastName", event.target.value)}
+        />
+        <label>Phone</label>
+        <input
+          type="text"
+          value={userInput.phoneNumber}
+          onChange={(event) => onChange("phoneNumber", event.target.value)}
+        />
+
+        <label>Email</label>
+        <input
+          type="email"
+          value={userInput.email}
           onChange={(event) => onChange("email", event.target.value)}
         />
       </div>
